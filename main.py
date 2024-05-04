0f8cdcb2ecb4aac5fb04b722cb1918d98fa59f21
Smoliak Sergei <spcx31@gmail.com>
2024-05-04 18:26:17 +0300

    Added task: 1 && 2

---
+++ main.py
@@ -0,0 +1,15 @@
+from tasks.task_1 import copy_dir_content
+from tasks.task_2 import start_draw_koch_snowflake
+
+''''Task 1'''
+''''Please set path in variable: source_dir & dest_dir'''
+"""Uncomment me"""
+# source_dir = ""
+# dest_dir = ""
+# copy_dir_content(source_dir, dest_dir)
+
+
+''''Task 2'''
+''''Please after start enter depth. Example: 4'''
+"""Uncomment me"""
+# start_draw_koch_snowflake()
